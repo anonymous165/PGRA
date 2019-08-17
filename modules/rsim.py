e8085a0e96b0e580e7665149a604b221b00b0cb8
anon <->
2019-08-17 12:05:48 +0900

    Update rsim.py

--- modules/rsim.py
+++ modules/rsim.py
@@ -5,9 +5,4 @@ from torch.nn import functional as F
 class CosineSim(torch.nn.Module):
 
     def forward(self, a, b):
-        # return (a*b).sum(-1)
-        # return torch.abs((F.normalize(a, dim=-1) * F.normalize(b, dim=-1)).sum(dim=-1))
-
-        # return ((F.normalize(a, dim=-1) * F.normalize(b, dim=-1)).sum(dim=-1) + 1) / 2
-        # return torch.abs(a * b).sum(dim=-1)
         return F.relu((F.normalize(a, dim=-1) * F.normalize(b, dim=-1)).sum(dim=-1))
